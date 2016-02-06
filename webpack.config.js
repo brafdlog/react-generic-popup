644e99f4acb5f0298d43f091de8cac698bb8080d
Jonathan <jonathan@dudamobile.com>
2016-02-06 21:52:45 +0200

    Use gulp for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  entry: './src/index.js',
   output: {
     // Name of the variable on the window object (in case we target browser only)
     library: 'reactGenericPopup',
     libraryTarget: 'umd',
     // Output file name
-    filename: 'lib/reactGenericPopup.js'
+    filename: 'reactGenericPopup.js'
   },
   module: {
     loaders: [
