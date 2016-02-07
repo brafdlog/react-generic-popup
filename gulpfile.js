00eeee3cd3663535d77ceb815f10778bf965e42f
Jonathan <jonathan@dudamobile.com>
2016-02-07 09:47:02 +0200

    Add default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 
+gulp.task('default', ['build']);
+
 gulp.task('build', function() {
   return gulp.src('src/index.js')
     .pipe(webpack(require('./webpack.config.js')))
